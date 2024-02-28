40cdd4f66edd8591f51d65f92677d082b096e5f2
botto131 <brandonotto131@gmail.com>
2024-02-28 15:47:06 -0600

    second try

---
+++ src/main/java/pet/store/entity/Customer.java
@@ -0,0 +1,41 @@
+package pet.store.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+
+@Data
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+    private Long customerId;
+    
+    @Column(name = "customer_first_name")
+    private String customerFirstName;
+    
+    @Column(name = "customer_last_name")
+    private String customerLastName;
+    
+    @Column(name = "customer_email")
+    private String customerEmail;
+    
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude    
+    @ManyToMany(mappedBy = "customers", cascade = CascadeType.PERSIST)
+    Set<PetStore> petStores;
+    
+}
+
+
